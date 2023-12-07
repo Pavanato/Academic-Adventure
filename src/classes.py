e2cbc937cf0902ed7e0ea2ed1ff0f50fb2f50766
Pavanato <tandera837@hotmail.com>
2023-12-07 03:40:01 -0300

    improved classes.py, removed testing files, overall organization

--- src/classes.py
+++ src/classes.py
@@ -1,26 +1,58 @@
 import pygame
 from os import walk
 from settings import *
-
-# TODO: Remove unnecessary import of sys
+import math
+import time
 import sys
 
-# TODO: Create screen, clock, BG for the main menu. Check if these can be removed
+
+# TODO: Create screen, clock for the main menu. Check if these can be removed
 screen = pygame.display.set_mode((screen_width, screen_height))
+# TODO: Create the function Game() that has the clock as an entrie
 clock = pygame.time.Clock()
-BG = pygame.image.load("graphics/button/Background.png")
 
-# TODO: Move this function to a more appropriate place
+# Auxiliary functions
 def get_font(size):
-    return pygame.font.Font("graphics/button/font.ttf", size)
+    return pygame.font.Font(r"src\graphics\button\font.ttf", size)
 
+def import_folder(path):
+    surface_list = []
+
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            try:
+                image_surf = pygame.image.load(full_path).convert_alpha()
+                surface_list.append(image_surf)
+            except pygame.error as e:
+                print(f"Error loading image: {full_path} - {e}")
+
+    return surface_list
+
+# Classes
 class Entity(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.rect = pygame.Rect(pos[0], pos[1], 0, 0)
+        self.x_speed = 2
+        self.y_speed = 2
 
     def update(self):
-        pass
+        # Move the entity along the x-axis and check for collisions
+        self.rect.x += self.x_speed
+        for tile in pygame.sprite.spritecollide(self, tiles_group, False):
+            if self.x_speed > 0:  # Moving right
+                self.rect.right = tile.rect.left
+            elif self.x_speed < 0:  # Moving left
+                self.rect.left = tile.rect.right
+
+        # Move the entity along the y-axis and check for collisions
+        self.rect.y += self.y_speed
+        for tile in pygame.sprite.spritecollide(self, tiles_group, False):
+            if self.y_speed > 0:  # Moving down
+                self.rect.bottom = tile.rect.top
+            elif self.y_speed < 0:  # Moving up
+                self.rect.top = tile.rect.bottom
 
 class Tile(Entity):
     def __init__(self, pos, size):
@@ -54,7 +86,7 @@ class Player(Entity):
         self.on_right = False
 
     def import_character_assets(self):
-        character_path = 'graphics/character/'
+        character_path = 'src/graphics/character/'
         
         self.animations = {'idle_direita':[],'idle_esquerda':[], 'fall':[], 'menino_andando_direita':[], 'menino_andando_esquerda':[], 'run':[], 'jump':[]}
 
@@ -117,6 +149,41 @@ class Player(Entity):
 
         self.image = animation[int(self.frame_index)]
 
+class NPC(Entity):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.image = pygame.Surface((50, 50))  # Set the size of the enemy
+        self.image.fill('blue')
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        self.speed = 2
+        self.original_direction = self.speed
+
+    def update(self, x_shift):
+        # Simple movement pattern: move right until hitting a wall, then stop
+        self.rect.x += self.speed + x_shift
+        if self.rect.right > screen_width or self.rect.left < 0:
+            self.speed = 0
+        elif self.speed == 0:
+            self.speed = self.original_direction
+
+class Enemy(Entity):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.image = pygame.Surface((50, 50))  # Set the size of the enemy
+        self.image.fill('red')  # Set the color of the enemy
+        self.rect = self.image.get_rect(topleft=pos)
+        self.start_time = time.time()  # Record the time when the enemy is created
+        self.speed = 5  # The speed at which the enemy moves
+
+    def update(self, x_shift):
+        # Calculate the elapsed time since the enemy was created
+        elapsed_time = time.time() - self.start_time
+
+        # Calculate the direction of movement based on the elapsed time
+        direction = math.sin(elapsed_time * math.pi)
+
+        # Move the enemy
+        self.rect.x += self.speed * direction + x_shift
 
 class Level:
     def __init__(self, level_data, surface):
@@ -128,6 +195,8 @@ class Level:
     def initialize_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
+        self.enemies = pygame.sprite.Group()
+        self.npcs = pygame.sprite.Group()
 
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
@@ -141,13 +210,19 @@ class Level:
                 if cell == 'P':
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
+                if cell == 'E':
+                    enemy_sprite = Enemy((x, y))
+                    self.enemies.add(enemy_sprite)
+                if cell == 'N':
+                    npc_sprite = NPC((x, y))
+                    self.npcs.add(npc_sprite)
 
     def scroll_x(self):
         player = self.player.sprite
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        screen_width = 1200
+        screen_width = 1280
         if player_x < screen_width / 4 and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
@@ -178,6 +253,7 @@ class Level:
         if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
             player.on_right = False
 
+
     def vertical_movement_collision(self):
         player = self.player.sprite
         player.apply_gravity()
@@ -198,6 +274,27 @@ class Level:
         if player.on_ceiling and player.direction.y > 0:
             player.on_ceiling = False
 
+    def npc_horizontal_movement_collision(self, npc):
+        npc.rect.x += npc.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(npc.rect):
+                if npc.speed < 0:
+                    npc.rect.left = sprite.rect.right
+                    npc.speed = -npc.speed  # Reverse direction
+                elif npc.speed > 0:
+                    npc.rect.right = sprite.rect.left
+                    npc.speed = -npc.speed  # Reverse direction
+    
+    def check_npc_collision(self):
+        # Check for collisions between the player and each NPC
+        for npc in self.npcs.sprites():
+            if pygame.sprite.collide_rect(self.player.sprite, npc):
+                # If a collision is detected, display a text box
+                font = pygame.font.Font(None, 36)
+                text = font.render("You collided with an NPC!", True, (255, 255, 255))
+                self.display_surface.blit(text, (200, 200))
+
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
@@ -208,19 +305,15 @@ class Level:
         self.vertical_movement_collision()
         self.player.draw(self.display_surface)
 
-def import_folder(path):
-    surface_list = []
+        self.enemies.update(self.world_shift)
+        self.enemies.draw(self.display_surface)
 
-    for _, __, img_files in walk(path):
-        for image in img_files:
-            full_path = path + '/' + image
-            try:
-                image_surf = pygame.image.load(full_path).convert_alpha()
-                surface_list.append(image_surf)
-            except pygame.error as e:
-                print(f"Error loading image: {full_path} - {e}")
-
-    return surface_list
+        self.npcs.update(self.world_shift)
+        for npc in self.npcs.sprites():
+            self.npc_horizontal_movement_collision(npc)
+        self.npcs.draw(self.display_surface)
+        self.check_npc_collision()
+        pygame.display.flip()
 
 class Button:
     def __init__(self, image, pos, text_input, font, base_color, hovering_color):
@@ -305,11 +398,11 @@ class Menu:
             menu_text = get_font(30).render("Academic Adventure: From ABC to PhD", True, "#b68f40")
             menu_rect = menu_text.get_rect(center=(640, 100))
 
-            play_button = Button(image=pygame.image.load("graphics/button/Play Rect.png"), pos=(640, 250),
+            play_button = Button(image=pygame.image.load("src/graphics/button/Play Rect.png"), pos=(640, 250),
                                  text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-            options_button = Button(image=pygame.image.load("graphics/button/Options Rect.png"), pos=(640, 400),
+            options_button = Button(image=pygame.image.load("src/graphics/button/Options Rect.png"), pos=(640, 400),
                                     text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-            quit_button = Button(image=pygame.image.load("graphics/button/Quit Rect.png"), pos=(640, 550),
+            quit_button = Button(image=pygame.image.load("src/graphics/button/Quit Rect.png"), pos=(640, 550),
                                  text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
             screen.blit(menu_text, menu_rect)
@@ -335,18 +428,53 @@ class Menu:
 
             pygame.display.update()
 
-
+# Main
 if __name__ == "__main__":
+
+    # Initialize pygame
+    pygame.init()
+
+    # Initialize the font module separately
+    pygame.font.init()
+
+    # Now you can use pygame's font module
+    font = pygame.font.Font("graphics/button/font.ttf", 30)
+
     # Example usage of the classes
     level_data = [
-        "XXXXXXXXXXXXXXXXXXXXXX",
-        "X                    X",
-        "X                    X",
-        "X           P        X",
-        "XXXXXXXXXXXXXXXXXXXXXX",
+        "XXXXXXXXXXXXXXXXXXXX",
+        "X                  X",
+        "X         P        X",
+        "X                  X",
+        "X      X           X",
+        "X                  X",
+        "X          E       X",
+        "X     XE           X",
+        "X                  X",
+        "X           E      X",
+        "XXXXXXXXXXXXXXXXXXXX",
     ]
 
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
     game_level = Level(level_data, screen)
+
+
+    SCREEN = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("Menu")
+
     menu = Menu(game_level)
 
-    menu.main_menu()
+    menu.main_menu(background_image_path=r"graphics\button\Background.png")
+
+
+
+    #Q: any recomendation in this code?
+#R: Yes, you can use a dictionary to store the buttons and their functions, like this:
+#   buttons = {"Play": play, "Options": options, "Quit": quit}
+#   Then you can iterate over the buttons and check if the mouse is over the button
+#   for button in buttons:
+#       if button.check_for_input(mouse_pos):
+#           buttons[button]() # Call the function associated with the button
+#   This way you can add new buttons without having to change the code that checks for input
+#   and you can also add new functions without having to change the code that checks for input
